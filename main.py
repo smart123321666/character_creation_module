f24156c6ea81cb676a97f9108e04bc6edac6ea76
Даниил Соколов <scake14@yandex.ru>
2023-07-06 18:21:40 +0500

    task7-2

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
+from random import randint
+
+print(randint.__doc__)
+
+
 def attack(char_name: str, char_class: str) -> str:
+    """
+    Выполняет атаку для персонажа.
+
+    Аргументы:
+        char_name (str): Имя персонажа.
+        char_class (str): Класс персонажа.
+
+    Возвращает:
+        str: Сообщение, указывающее результат атаки.
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный'
                 f'{5 + randint(3, 5)}')
@@ -15,6 +30,16 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """
+    Выполняет защиту для персонажа.
+
+    Аргументы:
+        char_name (str): Имя персонажа.
+        char_class (str): Класс персонажа.
+
+    Возвращает:
+        str: Сообщение, указывающее результат защиты.
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -25,6 +50,16 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """
+    Применяет специальное умение для персонажа.
+
+    Аргументы:
+        char_name (str): Имя персонажа.
+        char_class (str): Класс персонажа.
+
+    Возвращает:
+        str: Сообщение, указывающее результат применения специального умения.
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил'
                 f'специальное умение «Выносливость {80 + 25}»')
@@ -38,6 +73,16 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """
+    Начинает тренировку для выбранного персонажа.
+
+    Аргументы:
+        char_name (str): Имя персонажа.
+        char_class (str): Класс персонажа.
+
+    Возвращает:
+        str: Сообщение, указывающее окончание тренировки.
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -62,6 +107,12 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """
+    Позволяет выбрать класс персонажа.
+
+    Возвращает:
+        str: Выбранный класс персонажа.
+    """
     char_class: str = ''
     approve_choice: str = ''
     while approve_choice != 'y':
@@ -83,7 +134,7 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
     run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
@@ -94,5 +145,3 @@ def main() -> None:
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-main()
